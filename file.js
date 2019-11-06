658b4d532ac9aa088af4ff9af128f5a0355ef296
Emily Hernandez <emily.hernandez@ukkopro.fi>
2019-11-06 12:59:55 +0200

    Turn classes into functions to use private nested functions

--- file.js
+++ file.js
@@ -1,35 +1,18 @@
-class File {
-  constructor(file, id) {
-    this.id = id
-    this.file = ''
-    this.objectURL = URL.createObjectURL(file)
-    this.fileName = file.name
-    this.fileExtension = this.extractFileExtension(file.name)
-    this.mimeType = file.type
-    this.fileSize = file.size
-    this.readFile = this.readFile.bind(this)
-    this.readFile(file)
-  }
+'use strict'
 
-  extractFileExtension(fileName) {
-    if (fileName.length > 0) {
-      const index = fileName.lastIndexOf('.')
-      return index > -1 ? fileName.slice(index) : ''
-    }
-    return ''
+function extractFileExtension(fileName) {
+  if (fileName.length > 0) {
+    const index = fileName.lastIndexOf('.')
+    return index > -1 ? fileName.slice(index) : ''
   }
-
-  readFile(URL) {
-    let reader = new FileReader()
-    reader.addEventListener('load', event => {
-      this.file = event.target.result
-    })
-    reader.readAsDataURL(URL)
-
-    reader.addEventListener('load', event => {
-      this.objectURL = event.target.result
-    })
-  }
+  return ''
 }
 
-module.exports = File
+module.exports = function (file, id) {
+  this.id = id
+  this.file = file
+  this.fileName = file.name
+  this.fileExtension = extractFileExtension(file.name)
+  this.mimeType = file.type
+  this.fileSize = file.size
+}
--- fileupload.js
+++ fileupload.js
@@ -1,58 +1,50 @@
-const File = require('./file')
+'use strict'
 
-module.exports = class FileUploader {
-  /**
-   * Creates an instance of {@link FileUploader}.
-   *
-   * @constructor
-   * @param {object} options The custom options for {@link FileUploader}.
-   */
-  constructor(options) {
-    this.options = {
-      field: null,
-      dropzone: null,
-      onAddFile: () => { },
-      onRemoveFile: () => { },
-      accept: '',
-      allowDrop: false,
-      name: 'file-upload-input',
-      labelClass: 'file-uploader-label',
-      fieldClass: 'file-uploader-input'
-    }
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
+}
+module.exports = function (options) {
+  this.options = {
+    field: null,
+    dropzone: null,
+    multiple: true,
+    onAddFile: () => { },
+    onRemoveFile: () => { },
+    accept: '',
+    allowDrop: false,
+    name: 'file-upload-input',
+    labelClass: 'file-uploader-label',
+    fieldClass: 'file-uploader-input'
+  }
+  this.options = Object.assign(this.options, options)
+  this.field = null
+  this.fileList = []
 
-    // Properties
-    this.options = Object.assign(this.options, options)
-    this.field = null
-    this.fileList = []
+  this.add = function (files) {
+    if (this.fileList.length > 0) this.fileList = []
+    const addedFiles = new File(files[0], this.fileList.length + 1)
 
-    // Methods-->
-    this.add = this.add.bind(this)
-    this.remove = this.remove.bind(this)
-    this.drop = this.drop.bind(this)
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.config = this.config.bind(this)
+    this.fileList.push(addedFiles)
+    this.field.setAttribute('files', this.fileList)
 
-    // Hooks -->
-    this.onAddFile = this.options.onAddFile.bind(this)
-    this.onRemoveFile = this.options.onRemoveFile.bind(this)
-
-    this.config()
+    this.options.onAddFile(addedFiles)
   }
 
-  /**
-   * Sets custom options on top of default options of the instance of {@link FileUploader}.
-   *
-   * @function
-   * @param {object} options The custom options for {@link FileUploader} passed to constructor.
-   */
-  config() {
+  this.remove = function () {
+    this.fileList = []
+    this.options.onRemoveFile()
+  }
+
+  const config = function () {
+    console.log('this', this)
+
     this.field =
       this.options.field && this.options.field.nodeName
         ? this.options.field
         : null
 
     // set up the field
-    this.field.addEventListener('change', this.handleInputChange)
+    this.field.addEventListener('change', handleAddEvent)
     this.field.setAttribute('class', this.options.fieldClass)
     this.field.setAttribute('accept', this.options.accept)
 
@@ -65,24 +57,23 @@ module.exports = class FileUploader {
       this.field.click()
     })
     label.innerHTML = 'Add files'
-    this.insertAfter(label, this.field)
+    insertAfter(label, this.field)
 
     if (this.options.allowDrop) {
-
       if (this.options.dropzone === null) {
         this.options.dropzone = document.createElement('div')
-        this.insertAfter(this.options.dropzone, document.querySelector(`label[for=${this.options.name}]`))
+        this.insertAfter(
+          this.options.dropzone,
+          document.querySelector(`label[for=${this.options.name}]`)
+        )
       }
       /*
       e.preventDefault() and e.stopPropagation() prevent any unwanted behaviors for the assigned events across browsers.
       */
-      this.options.dropzone.addEventListener(
-        'dragover',
-        event => {
-          event.stopPropagation()
-          event.preventDefault()
-        }
-      )
+      this.options.dropzone.addEventListener('dragover', event => {
+        event.stopPropagation()
+        event.preventDefault()
+      })
 
       this.options.dropzone.addEventListener('drag', event => {
         event.stopPropagation()
@@ -92,62 +83,20 @@ module.exports = class FileUploader {
       /*
       e.originalEvent.dataTransfer.files returns the list of files that were dropped.
       */
-      this.options.dropzone.addEventListener('drop', this.drop)
+      this.options.dropzone.addEventListener('drop', drop)
       this.options.dropzone.setAttribute('class', 'file-uploader-dropzone')
     }
-  }
+  }.bind(this)
 
-  /**
-   * Updates the the fileList property and fires onAddFile callback function.
-   *
-   * @function
-   * @param {object} event The input event.
-   */
-  handleInputChange(event) {
+  const handleAddEvent = function (event) {
     // if file already exists, clear the fileList
     // @NOTE: if multiple file loading is later supported, this logic will need to be updated
     this.add(event.target.files)
-  }
+  }.bind(this)
 
-  add(files) {
-    if (this.fileList.length > 0) this.fileList = []
-    var addedFiles = new File(files[0], this.fileList.length + 1)
-
-    this.fileList.push(addedFiles)
-    this.field.setAttribute('files', this.fileList)
-
-    this.onAddFile(addedFiles)
-  }
-
-  remove() {
-    this.fileList = []
-
-    this.onRemoveFile()
-  }
-
-  drop(event) {
+  const drop = function (event) {
     this.add(event.dataTransfer.files)
-  }
+  }.bind(this)
 
-  insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling)
-  }
-
-  /**
-   * Returns the 'files' attribute of the field.
-   *
-   * @return {object} A FileList object that lists every selected file. This list has no more than one member unless the 'multiple' attribute is specified.
-   */
-  get files() {
-    return this.field.files
-  }
-
-  /**
-   * Returns the 'value' attribute ot the field.
-   *
-   * @return {string} A DOMString representing the path to the selected file..
-   */
-  get value() {
-    return this.field.value
-  }
+  config()
 }
